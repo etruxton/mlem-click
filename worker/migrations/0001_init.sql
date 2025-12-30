54ee6c31b46f7d4ef322d1a3afe99649f9eaa34c
etruxton <awesomeerica743@gmail.com>
2025-12-30 16:40:21 -0600

    update worker and fix db migration job

--- worker/migrations/0001_init.sql
+++ worker/migrations/0001_init.sql
@@ -4,5 +4,5 @@ CREATE TABLE IF NOT EXISTS clicks (
     total INTEGER NOT NULL DEFAULT 0
 );
 
--- Initialize with a single row for the counter
-INSERT INTO clicks (id, total) VALUES (1, 0);
+-- Initialize with a single row for the counter (ignore if already exists)
+INSERT OR IGNORE INTO clicks (id, total) VALUES (1, 0);
